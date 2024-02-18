c467165f5a793898b1a45c79b9271c5c6ed529e5
kone4580 <yokoyama@i-square.jp>
2024-02-18 14:44:05 +0900

    gate

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.scss',
+                'resources/css/app.css',
                 'resources/scss/styles.scss',
                 'resources/js/app.js',
             ],
