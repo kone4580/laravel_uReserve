32833458400c4ffb3b9578a8881704ee61fbc97a
kone4580 <yokoyama@i-square.jp>
2024-02-18 14:09:45 +0900

    logo

--- resources/views/components/authentication-card.blade.php
+++ resources/views/components/authentication-card.blade.php
@@ -1,9 +1,23 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-    <div>
+<div class="login_section page_section__gray">
+    <div class="header_logo">
         {{ $logo }}
     </div>
 
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-        {{ $slot }}
+    <div class="login-box">
+        <div class="login-form">
+            {{ $slot }}
+        </div>
     </div>
+
+    <a href="../signin/" class="register-btn">
+        はじめてご利用の方
+        <img src="../assets/images/common/angle_right_white.png" class="angle_right_white" alt="">
+    </a>
+
+    <ul class="useful_link">
+        <li><a href="../terms/">利用規約</a></li>
+        <li><a href="../privacy/">プライバシーポリシー</a></li>
+    </ul>
+
+    <address class="copyright">© 2023 ISHIGAKIJIMA-ACTIBITY All Rights Reserved.</address>
 </div>
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'resources/css/app.scss',
+                'resources/scss/styles.scss',
                 'resources/js/app.js',
             ],
             refresh: [
